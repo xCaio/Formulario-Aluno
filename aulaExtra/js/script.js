4d44cd1cc65d5843908d8aa2ee570f722eb609f6
Caio Santos <50676424+xCaio@users.noreply.github.com>
2022-03-06 14:16:15 -0300

    Update script.js

--- aulaExtra/js/script.js
+++ aulaExtra/js/script.js
@@ -1,34 +1,74 @@
-var nomeAluno;
-
-const confirmar = document.querySelector(".buttons #confirmar");
-const cancelar = document.querySelector("#cancelar");
-const todaSessao = document.querySelector("section");
-
-//PEGANDO VALOR DO INPUT
-const nomeValor = document.querySelector("#nome").value;
-// const dataValor = document.querySelector("#data").value;
-// const horaInicialValor = document.querySelector("#horaInicial").value
-// const horaFinalValor = document.querySelector("#horaFinal").value;
-// // const materiaValor = document.querySelector("materia").value;
-// const dificuldadeValor = document.querySelector("#select").value;
-
-
 function verificaAluno(){
-  const nomeAluno = document.querySelector("#aluno");
-  const entrada = document.querySelector("input").value;
-  
-
+  const nomeAluno = document.querySelector("#alunoTable");
+  const entrada = document.querySelector("input#nome").value;
   nomeAluno.textContent = entrada;
+   
+  const dataValor = document.querySelector("#resData");
+  const dataResultado = document.querySelector("#dataInicio").value;
+  dataValor.textContent = dataResultado;
+
+  const materia = document.querySelector("#materiaTable");
+  const entradaMateria = document.querySelector("input#materia").value;
+  materia.textContent = entradaMateria;
+
+  const horaInicial = document.querySelector("#horaInicial")
+  const entradaHoraInicial = document.querySelector("#horaInicialInput").value;
+  horaInicial.textContent = entradaHoraInicial;
+  
+  const horaFinal = document.querySelector("#horaFinal")
+  const entradaHoraFinal = document.querySelector("#horaFinalInput").value;
+  horaFinal.textContent = entradaHoraFinal;
+
+  const dificuldade = document.querySelector("#dificuldade");
+  const entradaDificuldade = document.querySelector(".form-select").value;
+  dificuldade.textContent = entradaDificuldade;
+
+  if(entradaDificuldade == 1){
+    dificuldade.textContent = "Fácil";
+  }else if(entradaDificuldade == 2){
+    dificuldade.textContent = "Médio";
+  }else if(entradaDificuldade == 3){
+    dificuldade.textContent = "Difícil";
+  }else{
+    dificuldade.textContent = "Selecione";
+  }
 
 }
 
 
-
 function limparDados(){
-nomeValor = document.querySelector("#nome").remove;
-dataValor = document.querySelector("#data").remove;
-horaInicialValor = document.querySelector("#horaInicial").remove
-horaFinalValor = document.querySelector("#horaFinal").remove;
-materiaValor = document.querySelector("materia").remove;
-dificuldadeValor = document.querySelector("#select").remove;
+  const nomeAluno = document.querySelector("#alunoTable");
+  const entrada = document.querySelector("input#nome").value;
+  nomeAluno.textContent = entrada.remove;
+   
+  const dataValor = document.querySelector("#resData");
+  const dataResultado = document.querySelector("#dataInicio").value;
+  dataValor.textContent = dataResultado.remove;
+
+  const materia = document.querySelector("#materiaTable");
+  const entradaMateria = document.querySelector("input#materia").value;
+  materia.textContent = entradaMateria.remove;
+
+  const horaInicial = document.querySelector("#horaInicial")
+  const entradaHoraInicial = document.querySelector("#horaInicialInput").value;
+  horaInicial.textContent = entradaHoraInicial.remove;
+  
+  const horaFinal = document.querySelector("#horaFinal")
+  const entradaHoraFinal = document.querySelector("#horaFinalInput").value;
+  horaFinal.textContent = entradaHoraFinal.remove;
+
+  const dificuldade = document.querySelector("#dificuldade");
+  const entradaDificuldade = document.querySelector(".form-select").value;
+  dificuldade.textContent = entradaDificuldade.remove;
+
+  if(entradaDificuldade == 1){
+    dificuldade.textContent = "Fácil";
+  }else if(entradaDificuldade == 2){
+    dificuldade.textContent = "Médio";
+  }else if(entradaDificuldade == 3){
+    dificuldade.textContent = "Difícil";
+  }else{
+    dificuldade.textContent = "Selecione".remove;
+  }
+  
 }
